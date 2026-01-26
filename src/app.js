c45253e7440350da8a809ab562228bbfcb26bd90
trandainghia1005 <trandainghia.dev@gmail.com>
2026-01-26 14:28:06 +0000

    faet : check OverLoad Connection

--- src/app.js
+++ src/app.js
@@ -1,19 +1,21 @@
-const express = require('express')
-const morgan = require('morgan')
-const {default : helmet} = require('helmet')
-const compression = require('compression')
-const app = express()
+const express = require("express");
+const morgan = require("morgan");
+const { default: helmet } = require("helmet");
+const compression = require("compression");
+const app = express();
 
-app.use(morgan('dev'))
-app.use(helmet())
-app.use(compression())
-require('./dbs/init.mongoDB')
-app.use('/', (req, res, next) =>{
-    const strCom = 'hello wsl'
-    return res.status(200).json({
-        message:'Hello WSL ',
-        metadata:strCom.repeat(10000)
-    })
-})
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+require("./dbs/init.mongoDB");
+const { checkOverLoad } = require("./helpers/check.connect");
+checkOverLoad();
+app.use("/", (req, res, next) => {
+  const strCom = "hello wsl";
+  return res.status(200).json({
+    message: "Hello WSL ",
+    metadata: strCom.repeat(10000),
+  });
+});
 
-module.exports = app
+module.exports = app;
--- src/helpers/check.connect.js
+++ src/helpers/check.connect.js
@@ -1,10 +1,30 @@
 "use strict";
 
 const mongoose = require("mongoose");
-
+const process = require("process");
+const os = require("os");
+const _SECONDS = 5000;
+// count Connect
 const countConnect = () => {
   const numConnection = mongoose.connections.length;
   console.log(`Number of connection:${numConnection}`);
 };
+// check overload
+const checkOverLoad = () => {
+  setInterval(() => {
+    const numConnection = mongoose.connections.length;
 
-module.exports = { countConnect };
+    const numsCores = os.cpus().length;
+
+    const memoryUsage = process.memoryUsage().rss;
+    // example maximum number of connections based on number of cores
+    const maxConnections = numsCores * 5;
+
+    console.log(`Active connections: ${numConnection}`);
+    console.log(`Memory usage: ${memoryUsage / 1024 / 1024}MB`);
+    if (numConnection > maxConnections) {
+      console.log("Connection overload detected");
+    }
+  }, _SECONDS); // Monitor every 5 seconds
+};
+module.exports = { countConnect, checkOverLoad };
