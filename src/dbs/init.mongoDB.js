01c67e8da80cd4c18789b5e27fc19796e1bbaf8d
trandainghia1005 <trandainghia.dev@gmail.com>
2026-01-26 13:35:43 +0000

    feat: check connect DB

--- src/dbs/init.mongoDB.js
+++ src/dbs/init.mongoDB.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 //lưu địa chỉ db
 const connectString = `mongodb://localhost:27017/ecommerce_project`;
 // Singleton Pattern
-
+const { countConnect } = require("../helpers/check.connect");
 class Database {
   constructor() {
     this.connect();
@@ -22,7 +22,7 @@ class Database {
         //số lượng kết nối tối đa trong connection pool
         maxPoolSize: 50,
       })
-      .then((_) => console.log("Connected MongoDB Success"))
+      .then((_) => console.log("Connected MongoDB Success", countConnect()))
       .catch((err) => console.log("Error Connect:", err));
     // Lắng nghe events
     mongoose.connection.on("disconnected", () => {
